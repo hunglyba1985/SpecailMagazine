3357dcbc40e52d10282e4cece40a0ef8c520ee17
Hung Ly Ba <hunglb.ctv@gmail.com>
2016-09-03 09:01:29 +0700

    Initial Commit

---
+++ SpecialMagazine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpecialMagazine
+//
+//  Created by Macbook Pro on 9/3/16.
+//  Copyright © 2016 Macbook Pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SpecialMagazine/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SpecialMagazine
+//
+//  Created by Macbook Pro on 9/3/16.
+//  Copyright © 2016 Macbook Pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
